30d20d9254278d79ab2fee7301fd47b6d4958deb
The2ndOne3 <rydenfreak@gmail.com>
2012-08-16 20:12:27 -0600

    Committed initial release.

---
+++ README.md
@@ -0,0 +1,26 @@
+priority-bucket.js
+==================
+This is a small script to create a priority bucket, which functions like an array that allows you to arbitrarily define the order of item traversal.
+
+Usage
+=======
+Constructor:
+```javascript
+var bucket = new PriorityBucket();
+```
+
+Pushing Items:
+```javascript
+bucket.push(item); // Pushes with a default priority of zero.
+bucket.push(item2, 68);
+bucket.push(item3, 200);
+bucket.push(item4, -20);
+bucket.push(item5, 50); // Now, traversal order is: [item3, item2, item5, item, item4]
+```
+
+Bucket Traversal:
+```javascript
+bucket.each(function(item){
+	console.log('Look, it's '+item+'!');
+});
+```
---
+++ priority-bucket.js
@@ -0,0 +1,64 @@
+/**
+ * Creates a priority bucket.
+ * Priority buckets are essentially associative arrays that map a priority to an array of values.
+ * This is useful for iterating across an array in a specific order.
+ * Higher priority entries are traversed first.
+ */
+(function(){
+	"use strict";
+	
+	/**
+	 * Constructor
+	 */
+	window.PriorityBucket=function(){
+		// Create a bucket to hold all the prioritized items.
+		this.bucket={};
+	};
+	
+	/**
+	 * Push a value with a priority.
+	 * @param value mixed The value to push.
+	 * @param priority int The priority; defaults to zero. Higher priorities are traversed first.
+	 */
+	window.PriorityBucket.prototype.push=function(value,priority){
+		// Priorities default to zero.
+		if(priority===undefined){
+			priority=0;
+		}
+		// Make a new bucket if it doesn't already exist.
+		if(this.bucket[priority]===undefined){
+			this.bucket[priority]=[];
+		}
+		// Push the value.
+		this.bucket[priority].push(value);
+	};
+	
+	/**
+	 * Traverses the priority bucket with a callback.
+	 * @param func callable A function with which to traverse the bucket. It is passed the current item being traversed.
+	 */
+	window.PriorityBucket.prototype.each=function(func){
+		// Get all priorities.
+		// Polyfill for Object.keys
+		var priorities=[];
+		if(!Object.keys){
+			for(var priority in this){
+				priorities.push(priority);
+			}
+		}
+		else{
+			priorities=Object.keys(this.bucket);
+		}
+		// Sort by priority.
+		priorities.sort(function(a,b){
+			return b-a;
+		});
+		// Iterate by priority.
+		for(var i=0,lenA=priorities.length;i<lenA;i++){
+			var bucket=this.bucket[priorities[i]];
+			for(var j=0,lenB=bucket.length;j<lenB;j++){
+				func(bucket[j]);
+			}
+		}
+	};
+})();
